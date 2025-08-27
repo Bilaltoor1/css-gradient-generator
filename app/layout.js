20480266d50b7ca8dc0a0ac76d3086ebf64c714a
Bilal Toor <bilaltoor786786@gmail.com>
2025-08-26 21:44:23 -0700

    latest changes

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,8 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "./components/ThemeProvider";
-
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -75,6 +76,8 @@ export default function RootLayout({ children }) {
             }}
           />
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
